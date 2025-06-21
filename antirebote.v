b7d4f848d8ee1fd96fc9e9f42e331a927e2f1cd7
Giuliana Cagnola <98410646+GiuliCagnola@users.noreply.github.com>
2025-06-21 11:44:46 -0300

    Update antirebote.v

--- antirebote.v
+++ antirebote.v
@@ -1,31 +1,36 @@
-module antirebote(input wire clk, input wire rst, input wire btn_in, output reg btn:out);
+module antirebote (
+    input wire clk,
+    input wire rst,
+    input wire btn_in, //entrada inestable
+    output reg btn_out //salida filtrada
+);
 
-    //btn_in -> entrada inestable
-    //btn_out -> salida estable
-
-    reg[15:0] contador;
+    reg [15:0] contador;
     reg btn_sync;
+    parameter limite = 50000; //ajustar según frecuencia del reloj
 
-    parameter limite=50000; //ajuste según frecuencia del reloj 
-
-    always@(posedge clk or posedge rst) begin
-        if(rst) begin
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
             contador <= 0;
             btn_sync <= 0;
             btn_out <= 0;
         end
         else begin
-            if(btn_in == btn_sync) begin
-                //mismo valor -> incrementa el contador
-                contador <= contador+1;
-            else
-                btn_out <= btn_sync;
+            //sincronizar la entrada
+            btn_sync <= btn_in;
+
+            //debounce
+            if (btn_sync == btn_out) begin
+                contador <= 0; //resetear si no hay cambio
             end
             else begin
-                //cambió el valor -> reiniciar el contador
-                contador <= 0;
-                btn_sync <= btn_in;
+                contador <= contador + 1;
+                if (contador >= limite) begin
+                    btn_out <= btn_sync; //actualizar si supera el límite
+                    contador <= 0;
+                end
             end
         end
     end
-endmodule 
+
+endmodule
