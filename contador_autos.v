376fdb44235c6ae885aa2dc99854a25783f795ef
Giuliana Cagnola <98410646+GiuliCagnola@users.noreply.github.com>
2025-06-21 11:45:29 -0300

    Update contador_autos.v

--- contador_autos.v
+++ contador_autos.v
@@ -1,7 +1,7 @@
 module contador_autos(input wire entrada, input wire salida, input wire clk, input wire rst, output reg [2:0] autos);
 
     always@(posedge clk or posedge rst) begin
-        if(reset) autos <= 3'b000;
+        if(rst) autos <= 3'b000;
         else begin
             case({entrada, salida})
                 2'b10: if(autos < 3'b111) autos <= autos+1; //entrada
